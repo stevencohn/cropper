b57b18e7faa019a35f72e2ca68ff6488d0390e7b
Steven M. Cohn <steven_cohn@waters.com>
2020-09-08 20:25:59 -0400

    initial prototype

---
+++ Screen.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Cropper
+{
+    partial class Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.Picture = new System.Windows.Forms.PictureBox();
+			this.LoadImage = new System.Windows.Forms.Button();
+			this.AntTimer = new System.Windows.Forms.Timer(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Picture
+			// 
+			this.Picture.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.Picture.Cursor = System.Windows.Forms.Cursors.Cross;
+			this.Picture.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Picture.Location = new System.Drawing.Point(10, 10);
+			this.Picture.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Picture.Name = "Picture";
+			this.Picture.Size = new System.Drawing.Size(1104, 660);
+			this.Picture.TabIndex = 1;
+			this.Picture.TabStop = false;
+			this.Picture.Paint += new System.Windows.Forms.PaintEventHandler(this.Picture_Paint);
+			this.Picture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Picture_MouseDown);
+			this.Picture.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Picture_MouseMove);
+			this.Picture.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Picture_MouseUp);
+			// 
+			// LoadImage
+			// 
+			this.LoadImage.Location = new System.Drawing.Point(4, 8);
+			this.LoadImage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.LoadImage.Name = "LoadImage";
+			this.LoadImage.Size = new System.Drawing.Size(186, 40);
+			this.LoadImage.TabIndex = 4;
+			this.LoadImage.Text = "Load Image";
+			this.LoadImage.UseVisualStyleBackColor = true;
+			this.LoadImage.Click += new System.EventHandler(this.LoadImage_Click);
+			// 
+			// AntTimer
+			// 
+			this.AntTimer.Interval = 50;
+			this.AntTimer.Tick += new System.EventHandler(this.AntTimer_Tick);
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.Controls.Add(this.Picture);
+			this.panel1.Location = new System.Drawing.Point(12, 77);
+			this.panel1.Name = "panel1";
+			this.panel1.Padding = new System.Windows.Forms.Padding(10);
+			this.panel1.Size = new System.Drawing.Size(1124, 680);
+			this.panel1.TabIndex = 5;
+			// 
+			// panel2
+			// 
+			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel2.Controls.Add(this.LoadImage);
+			this.panel2.Location = new System.Drawing.Point(12, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1124, 59);
+			this.panel2.TabIndex = 6;
+			// 
+			// Screen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1148, 769);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Screen";
+			((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button LoadImage;
+        private System.Windows.Forms.Timer AntTimer;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+	}
+}
+
---
+++ Screen.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+
+//https://www.codeproject.com/articles/27748/marching-ants
+
+
+namespace Cropper
+{
+	public partial class Screen : Form
+	{
+		private enum MoveState
+		{
+			None,
+			Selecting,
+			Moving,
+			Sizing
+		}
+		private enum HandlePosition
+		{
+			TopLeft, Top, TopRight, Right, BottomRight, Bottom, BottomLeft, Left
+		}
+		private class SelectionHandle
+		{
+			public HandlePosition Position;
+			public RectangleF Bounds;
+		}
+
+		private const int ImageTop = 4;
+		private const int ImageLeft = 4;
+
+		private Point startPoint;
+		private Point endPoint;
+		private Rectangle selectionBounds;
+		private readonly Region selectionRegion;
+		private readonly GraphicsPath selectionPath;
+		private readonly Image background;
+
+		private Image image;
+		private Rectangle imageBounds;
+		private List<SelectionHandle> handles;
+		private SelectionHandle currentHandle;
+
+		private int antOffset;
+		private MoveState moveState;
+
+		public Screen()
+		{
+			InitializeComponent();
+
+			selectionRegion = new Region();
+			selectionRegion.MakeEmpty();
+			selectionPath = new GraphicsPath();
+			selectionPath.Reset();
+
+			Picture.Image = background = new Bitmap(Picture.Width, Picture.Height);
+
+			handles = new List<SelectionHandle>();
+			moveState = MoveState.None;
+
+			// temp
+			image = Image.FromFile(@"C:\Github\OneMore\Screenshots\CodeBox.jpg");
+			imageBounds = new Rectangle(ImageLeft, ImageTop, image.Width + ImageLeft, image.Height + ImageTop);
+		}
+
+		private void LoadImage_Click(object sender, EventArgs e)
+		{
+			var dialog = new OpenFileDialog();
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				image = Image.FromFile(dialog.FileName);
+				imageBounds = new Rectangle(ImageLeft, ImageTop, image.Width + ImageLeft, image.Height + ImageTop);
+			}
+
+			Picture.Refresh();
+		}
+
+		#region Paint
+		private void Picture_Paint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.DrawImageUnscaled(background, 0, 0);
+
+			if (image != null)
+			{
+				e.Graphics.DrawImageUnscaled(image, ImageLeft, ImageTop);
+			}
+
+			if (selectionRegion.IsEmpty(e.Graphics))
+			{
+				return;
+			}
+
+			using (var fill = new SolidBrush(Color.FromArgb(40, 0, 138, 244)))
+			{
+				// fill for selecting region
+				e.Graphics.FillRegion(fill, selectionRegion);
+			}
+
+			using (var pen = new Pen(Color.White, 1f))
+			{
+				pen.DashStyle = DashStyle.Dash;
+				pen.DashPattern = new float[2] { 3, 3 };
+				pen.DashOffset = antOffset;
+
+				// set up pen for the ants
+				using (var ant = new Bitmap(Picture.Width, Picture.Height))
+				{
+					using (var g = Graphics.FromImage(ant))
+					{
+						// region is magenta but we'll use that as our transparent color
+						g.Clear(Color.Magenta);
+
+						using (var outline = MakeOutlinePath())
+						{
+							g.DrawPath(Pens.Black, outline);
+							g.DrawPath(pen, outline);
+						}
+
+						g.FillRegion(Brushes.Magenta, selectionRegion);
+					}
+
+					// make center of ant region transparent
+					ant.MakeTransparent(Color.Magenta);
+
+					// draw the ants on the image
+					e.Graphics.DrawImageUnscaled(ant, 0, 0);
+				}
+
+				var bounds = selectionRegion.GetBounds(e.Graphics);
+
+				handles.Clear();
+
+				AddHandle(HandlePosition.TopLeft, bounds.Left, bounds.Top, e.Graphics);
+				AddHandle(HandlePosition.TopRight, bounds.Right, bounds.Top, e.Graphics);
+				AddHandle(HandlePosition.BottomRight, bounds.Right, bounds.Bottom, e.Graphics);
+				AddHandle(HandlePosition.BottomLeft, bounds.Left, bounds.Bottom, e.Graphics);
+
+				AddHandle(HandlePosition.Top, bounds.Left + ((bounds.Right - bounds.Left) / 2), bounds.Top, e.Graphics);
+				AddHandle(HandlePosition.Right, bounds.Right, bounds.Top + ((bounds.Bottom - bounds.Top) / 2), e.Graphics);
+				AddHandle(HandlePosition.Bottom, bounds.Left + ((bounds.Right - bounds.Left) / 2), bounds.Bottom, e.Graphics);
+				AddHandle(HandlePosition.Left, bounds.Left, bounds.Top + ((bounds.Bottom - bounds.Top) / 2), e.Graphics);
+			}
+		}
+
+		private void AddHandle(HandlePosition position, float x, float y, Graphics g)
+		{
+			var rectangle = new RectangleF(x - 3, y - 3, 6, 6);
+			g.DrawArc(Pens.Black, rectangle.Left, rectangle.Top, 6, 6, 0, 360);
+
+			rectangle.Inflate(6, 6);
+			handles.Add(new SelectionHandle
+			{
+				Position = position,
+				Bounds = rectangle
+			});
+		}
+
+		private void SetSelection(Rectangle rectangle)
+		{
+			selectionRegion.MakeEmpty();
+			selectionPath.Reset();
+
+			if (!rectangle.IsEmpty)
+			{
+				selectionPath.AddRectangle(rectangle);
+				selectionRegion.Union(selectionPath);
+			}
+		}
+
+		public GraphicsPath MakeOutlinePath()
+		{
+			var path = new GraphicsPath();
+			if (selectionPath.PointCount > 0)
+			{
+				path.AddPath(selectionPath, false);
+				path.Widen(Pens.White);
+			}
+			return path;
+		}
+		#endregion Paint
+
+		private void Picture_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (image == null)
+			{
+				return;
+			}
+
+			// did we just grab a handle?
+			if ((currentHandle = HitHandle(e.Location)) != null)
+			{
+				switch (currentHandle.Position)
+				{
+					case HandlePosition.Top:
+					case HandlePosition.Left:
+					case HandlePosition.TopLeft:
+						startPoint.X = selectionBounds.Right;
+						startPoint.Y = selectionBounds.Bottom;
+						break;
+
+					case HandlePosition.TopRight:
+						startPoint.X = selectionBounds.Left;
+						startPoint.Y = selectionBounds.Bottom;
+						break;
+
+					case HandlePosition.BottomLeft:
+						startPoint.X = selectionBounds.Right;
+						startPoint.Y = selectionBounds.Top;
+						break;
+
+					case HandlePosition.Bottom:
+					case HandlePosition.Right:
+					case HandlePosition.BottomRight:
+						startPoint.X = selectionBounds.Left;
+						startPoint.Y = selectionBounds.Top;
+						break;
+				}
+
+				moveState = MoveState.Sizing;
+				return;
+			}
+
+			// else starting a new region
+			if (imageBounds.Contains(e.Location))
+			{
+				startPoint.X = e.X;
+				startPoint.Y = e.Y;
+				selectionBounds = new Rectangle(e.X, e.Y, 0, 0);
+			}
+			else
+			{
+				startPoint.X = startPoint.Y = -1;
+				selectionBounds = Rectangle.Empty;
+			}
+
+			SetSelection(Rectangle.Empty);
+
+			if (!AntTimer.Enabled)
+			{
+				AntTimer.Start();
+			}
+
+			moveState = MoveState.Selecting;
+		}
+
+		private SelectionHandle HitHandle(Point location)
+		{
+			foreach (var handle in handles)
+			{
+				if (handle.Bounds.Contains(location))
+				{
+					return handle;
+				}
+			}
+
+			return null;
+		}
+
+		private void Picture_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (moveState == MoveState.Selecting)
+			{
+				SelectRegion(e.Location);
+			}
+			else if (moveState == MoveState.Sizing)
+			{
+				ResizeRegion(e.Location);
+			}
+			else
+			{
+				var handle = HitHandle(e.Location);
+				if (handle != null)
+				{
+					Picture.Cursor = Cursors.Hand;
+					return;
+				}
+
+				Picture.Cursor = Cursors.Cross;
+			}
+		}
+
+		private void SelectRegion(Point location)
+		{
+			ConstrainLocation(ref location);
+
+			// do we have an in-bounds start point yet?
+			if (!imageBounds.Contains(startPoint))
+			{
+				startPoint.X = location.X;
+				startPoint.Y = location.Y;
+			}
+
+			// new end point
+			endPoint.X = location.X;
+			endPoint.Y = location.Y;
+
+			selectionBounds = new Rectangle(
+					Math.Min(startPoint.X, endPoint.X),
+					Math.Min(startPoint.Y, endPoint.Y),
+					Math.Abs(startPoint.X - endPoint.X),
+					Math.Abs(startPoint.Y - endPoint.Y)
+					);
+
+			SetSelection(selectionBounds);
+			Picture.Refresh();
+		}
+
+		private void ResizeRegion(Point location)
+		{
+			ConstrainLocation(ref location);
+
+			switch (currentHandle.Position)
+			{
+				case HandlePosition.Top:
+					endPoint.X = selectionBounds.Left;
+					endPoint.Y = location.Y;
+					break;
+
+				case HandlePosition.Right:
+					endPoint.X = location.X;
+					endPoint.Y = selectionBounds.Bottom;
+					break;
+
+				case HandlePosition.Bottom:
+					endPoint.X = selectionBounds.Right;
+					endPoint.Y = location.Y;
+					break;
+
+				case HandlePosition.Left:
+					endPoint.X = location.X;
+					endPoint.Y = selectionBounds.Top;
+					break;
+
+				case HandlePosition.TopLeft:
+				case HandlePosition.TopRight:
+				case HandlePosition.BottomLeft:
+				case HandlePosition.BottomRight:
+					endPoint.X = location.X;
+					endPoint.Y = location.Y;
+					break;
+			}
+
+			selectionBounds = new Rectangle(
+					Math.Min(startPoint.X, endPoint.X),
+					Math.Min(startPoint.Y, endPoint.Y),
+					Math.Abs(startPoint.X - endPoint.X),
+					Math.Abs(startPoint.Y - endPoint.Y)
+					);
+
+			SetSelection(selectionBounds);
+			Picture.Refresh();
+		}
+
+		private void ConstrainLocation(ref Point location)
+		{
+			if (!imageBounds.Contains(location))
+			{
+				// force it into bounds
+				if (location.X < imageBounds.Left)
+				{
+					location.X = imageBounds.Left;
+				}
+				else if (location.X > imageBounds.Right)
+				{
+					location.X = imageBounds.Right;
+				}
+
+				if (location.Y < imageBounds.Top)
+				{
+					location.Y = imageBounds.Top;
+				}
+				else if (location.Y > imageBounds.Bottom)
+				{
+					location.Y = imageBounds.Bottom;
+				}
+			}
+		}
+
+		private void Picture_MouseUp(object sender, MouseEventArgs e)
+		{
+			if (image == null)
+			{
+				return;
+			}
+
+			if (moveState == MoveState.Selecting)
+			{
+				if (imageBounds.Contains(e.Location))
+				{
+					endPoint.X = e.X;
+					endPoint.Y = e.Y;
+				}
+
+				selectionBounds = new Rectangle(
+					Math.Min(startPoint.X, endPoint.X),
+					Math.Min(startPoint.Y, endPoint.Y),
+					Math.Abs(startPoint.X - endPoint.X),
+					Math.Abs(startPoint.Y - endPoint.Y)
+					);
+
+				SetSelection(selectionBounds);
+
+				if (selectionBounds.IsEmpty)
+				{
+					AntTimer.Stop();
+				}
+
+				Picture.Refresh();
+			}
+
+			moveState = MoveState.None;
+		}
+
+		private void AntTimer_Tick(object sender, EventArgs e)
+		{
+			antOffset--;
+			antOffset %= 6;
+			Picture.Refresh();
+		}
+	}
+}
